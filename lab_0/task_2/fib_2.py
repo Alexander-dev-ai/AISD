5017285b3d77a2f1565a750aa7e122e691c3e1a4
Alexandr Kuchin <kuchinam.ru@gmail.com>
2024-09-12 20:53:52 +0300

    task_2

---
+++ lab_0/task_2/fib_2.py
@@ -0,0 +1,18 @@
+with open('input.txt') as f:
+    n = f.readline()
+n = int(n)
+
+
+def calc_fib(n):
+    if n <= 1:
+        return n
+    else:
+        return calc_fib(n - 1) + calc_fib(n - 2)
+
+
+if n < 0 or n > 45:
+    print('Error')
+else:
+    fn = calc_fib(n)
+    with open('output.txt', 'w') as f1:
+        f1.write(str(fn))
