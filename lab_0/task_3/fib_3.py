7beb36eb112eb6a68b4746cc31cd6c226de211cc
Alexandr Kuchin <kuchinam.ru@gmail.com>
2024-09-13 03:42:17 +0300

    task_3, moderate task_2

--- lab_0/task_3/fib_3.py
+++ lab_0/task_3/fib_3.py
@@ -15,12 +15,12 @@ def calc_fib(n):
         return calc_fib(n - 1) + calc_fib(n - 2)
 
 
-if n < 0 or n > 45:
+if n < 0 or n > 10**7:
     print('Error')
 else:
     fn = calc_fib(n)
     with open('output.txt', 'w') as f1:
-        f1.write(str(fn))
+        f1.write(str(fn % 10))
 
 end_time = time.time()
 print('Время выполнения:', end_time - start_time)
