0b6f9370dc3137df3693130605d849efac0b562c
Alexandr Kuchin <kuchinam.ru@gmail.com>
2024-09-14 20:09:02 +0300

    moderate task_1 (4)

--- lab_0/task_1/4.py
+++ lab_0/task_1/4.py
@@ -1,6 +1,5 @@
 with open('input.txt') as f:
-    a = f.readline()
-    b = f.readline()
+    a, b = map(int, f.readline().split())
 
 with open('output.txt', 'w') as f1:
     if (-10**9 <= int(a) <= 10**9) and (-10**9 <= int(b) <= 10**9):
