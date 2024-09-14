7bf3b67b4f3048b4c6f1bad41496c38ecae70c82
Alexandr Kuchin <kuchinam.ru@gmail.com>
2024-09-14 18:26:39 +0300

    moderate task_1

--- lab_0/task_1/4.py
+++ lab_0/task_1/4.py
@@ -2,5 +2,8 @@ with open('input.txt') as f:
     a = f.readline()
     b = f.readline()
 
-with open('output.txt', 'w') as f1:
-    f1.write(str(int(a) + int(b)**2))
+if (-10**9 <= int(a) <= 10**9) and (-10**9 <= int(b) <= 10**9):
+    with open('output.txt', 'w') as f1:
+        f1.write(str(int(a) + int(b)**2))
+else:
+    print('Error')
