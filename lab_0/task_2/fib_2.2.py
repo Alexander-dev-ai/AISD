f5bbe7bf8a751d59739f9caa3e21edcec18aae74
Alexandr Kuchin <kuchinam.ru@gmail.com>
2024-09-15 13:41:58 +0300

    .

--- lab_0/task_2/fib_2.2.py
+++ lab_0/task_2/fib_2.2.py
@@ -10,6 +10,10 @@ f1 = 1
 with open('output.txt', 'w') as f:
     if n < 0 or n > 45:
         f.write('Error')
+    elif n == 1:
+        f.write('0')
+    elif n == 2:
+        f.write('1')
     else:
         for i in range(n - 2):
             f0, f1 = f1, f1 + f0
