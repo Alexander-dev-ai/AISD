5bf01df3afce1fa8218d3d3014c56763b0e70445
Alexandr Kuchin <kuchinam.ru@gmail.com>
2024-09-15 14:40:15 +0300

    .

--- lab_0/task_3/fib_3.py
+++ lab_0/task_3/fib_3.py
@@ -5,19 +5,19 @@ start_time = time.time()
 with open('input.txt') as f:
     n = int(f.readline())
 
-f0 = 0
-f1 = 1
+f_1 = 1
+f_2 = 1
 with open('output.txt', 'w') as f:
-    if n < 0 or n > 10**7:
+    if n < 0 or n > 10 ** 7:
         print('Error')
-    elif n == 1:
+    elif n == 0:
         f.write('0')
-    elif n == 2:
+    elif n == 1 or n == 2:
         f.write('1')
     else:
         for i in range(n - 2):
-            f0, f1 = f1, f1 + f0
-        f.write(str(f1 % 10))
+            f_1, f_2 = f_2, f_1 + f_2
+        f.write(str(f_2 % 10))
 
 end_time = time.time()
 print(f'Время выполнения кода: {end_time - start_time}')
