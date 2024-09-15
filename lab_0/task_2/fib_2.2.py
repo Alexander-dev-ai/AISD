739aaaa6d05be8b5251d2c5436ec67fc330cd9a2
Alexandr Kuchin <kuchinam.ru@gmail.com>
2024-09-15 13:15:32 +0300

    mod task_2

---
+++ lab_0/task_2/fib_2.2.py
@@ -0,0 +1,13 @@
+with open('input.txt') as f:
+    n = int(f.readline())
+
+
+f0 = 0
+f1 = 1
+if n < 0 and n > 45:
+    print('Error')
+else:
+    for i in range(n - 2):
+        f0, f1 = f1, f1 + f0
+    with open('output.txt', 'w') as f:
+        f.write(str(f1))
