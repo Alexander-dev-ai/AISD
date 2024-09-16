000e2a4d96693be5089df31bd3fe0f7ca13386ed
Alexandr Kuchin <kuchinam.ru@gmail.com>
2024-09-16 17:45:55 +0300

    [feat] add new

--- lab_0/task_1/3.py
+++ lab_0/task_1/3.py
@@ -2,7 +2,7 @@ with open('input.txt') as f:
     a, b = map(int, f.readline().split())
 
 with open('output.txt', 'w') as f1:
-    if (-10**9 <= int(a) <= 10**9) and (-10**9 <= int(b) <= 10**9):
+    if (-10**9 <= int(a) <= 10**9) or (-10**9 <= int(b) <= 10**9):
         f1.write(str(int(a) + int(b)))
     else:
         f1.write('Error')
